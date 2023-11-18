8767df44c8f995b0f38be77d6bed00176ddc80d5
esteban11519 <eladinof@unal.edu.co>
2023-11-18 00:38:54 -0500

    RTOS and blink

--- Software/HeartRespirationRate/HeartRespirationRate/HeartRespirationRate.ino
+++ Software/HeartRespirationRate/HeartRespirationRate/HeartRespirationRate.ino
@@ -1,35 +1,125 @@
-// C++ code
-//
+#include <Arduino_FreeRTOS.h>
+// Include mutex support
+#include <semphr.h>
+
 /*
-  Sweep
+   Declaring a global variable of type SemaphoreHandle_t
 
-  by BARRAGAN <http://barraganstudio.com>
-  This example code is in the public domain.
-
-  modified 8 Nov 2013  by Scott Fitzgerald
-  http://www.arduino.cc/en/Tutorial/Sweep
 */
-// Libraries
-#include <Servo.h>
+SemaphoreHandle_t myMutex;
 
 
-#define pinServo 3
+int statusLed = 0;
+// Define tasks
 
-int pos = 0;
+// Pulse rate 60 t0 100 beats per minute
+void TaskPulseRate( void *pvParameters );
+// 12 to 16 breaths per minute
+void TaskRespirationRate( void *pvParameters );
 
-Servo servo;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  Serial.begin(9600);
 
-void setup()
-{
-  // https://components101.com/motors/mg995-servo-motor
-  servo.attach(pinServo, 500, 2500);
+  /**
+       Create a mutex.
+       https://www.freertos.org/CreateMutex.html
+  */
+  myMutex = xSemaphoreCreateMutex();
+  if (myMutex != NULL) {
+    Serial.println("Mutex created");
+  }
+  
+  // initialize digital LED_BUILTIN on pin 13 as an output.
+  
+
+  // Now set up two tasks to run independently.
+  xTaskCreate(
+    TaskRespirationRate
+    ,  "RespirationRate"   // A name just for humans
+    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL );
+
+  xTaskCreate(
+    TaskPulseRate
+    ,  "PulseRate"
+    ,  128  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
 void loop()
-{  
-  servo.write(75);
-  delay(100);
-  servo.write(105);
-  delay(100);
-  
+{
+  // Empty. Things are done in Tasks.
 }
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void TaskRespirationRate(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+  // initialize digital LED_BUILTIN on pin 13 as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  int respirationRate = 1000; // milliseconds
+  
+  // obtenemos la primer referencia temporal
+  TickType_t lastWakeupRespirationRate = xTaskGetTickCount();
+  
+
+  for (;;) // A Task shall never return or exit.
+  {
+    // pdMS_TO_TICKS: Milliseconds to ticks
+    vTaskDelayUntil( &lastWakeupRespirationRate, pdMS_TO_TICKS( respirationRate ) );
+    
+    // Task in Mutex
+    xSemaphoreTake(myMutex,portMAX_DELAY);
+    digitalWrite(LED_BUILTIN, statusLed);
+    statusLed = !statusLed;
+    xSemaphoreGive(myMutex);
+    
+    
+  }
+}
+
+void TaskPulseRate(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+  int pulseRate = 300; // milliseconds
+   
+  // obtenemos la primer referencia temporal
+  TickType_t lastWakeupPulseRate = xTaskGetTickCount();
+  
+
+  for (;;) // A Task shall never return or exit.
+  {
+    vTaskDelayUntil( &lastWakeupPulseRate, pdMS_TO_TICKS( pulseRate ) );
+    
+    // Task in Mutex
+    xSemaphoreTake(myMutex,portMAX_DELAY);
+    digitalWrite(LED_BUILTIN, statusLed);
+    statusLed = !statusLed;
+    xSemaphoreGive(myMutex);
+    
+    
+  }
+}
+
+// References
+/*
+https://www.hopkinsmedicine.org/health/conditions-and-diseases/vital-signs-body-temperature-pulse-rate-respiration-rate-blood-pressure
+https://www.youtube.com/watch?v=U5Vep8vjcEQ
+https://circuitdigest.com/microcontroller-projects/arduino-freertos-tutorial-using-semaphore-and-mutex-in-freertos-with-arduino
+vTaskDelayUntil: 
+- http://fjrg76.com/2021/09/30/funciones-de-retardo-en-freertos/
+- https://www.freertos.org/vtaskdelayuntil.html
+
+*/
